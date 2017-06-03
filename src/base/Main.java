76bed0229cebf731fda2bf3202246924f8722328
Oifa <linfli@yandex.com>
2017-06-03 20:25:19 +1000

    music player

--- src/base/Main.java
+++ src/base/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("base.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Music");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
